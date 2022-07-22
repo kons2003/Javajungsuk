dfeaa63203edf51767e3b8be53a6f10e3d7549dd
kons2003 <mj030313@naver.com>
2022-07-22 20:34:45 +0900

    init commit

---
+++ workspace/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,9 @@
+
+K
+src/ch2/src/CastingEx1.java,d\2\d2c591745b2331c72465bfb56e27f72fda38598b
+F
+src/ch1/src/Hello.java,5\b\5b6f2a0cf89091eb740c2e93df2173815a990867
+J
+src/ch5/src/ArrayEx15.java,b\2\b249cd4c718c29158b82da03f18f1845e3c0f25e
+H
+src/ch4/src/FlowEx1.java,8\c\8cce5025ed9fe1034bb0857afb104debcb3ae8d2
---
+++ workspace/src/ch2/src/FloatEx1.java
@@ -0,0 +1,20 @@
+import java.math.BigDecimal;
+
+public class FloatEx1 {
+    public static void main(String[] args) {
+        float f = 9.12345678901234567890f;
+        float f2 = 1.2345678901234567890f;
+        double d = 9.12345678901234567890d;
+        BigDecimal b = new BigDecimal(9.1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890);
+
+        System.out.printf("        123456789012345678901234%n");
+        System.out.printf("f: %f%n", f);  // 소수점 이하 6째자리까지 출력.
+        System.out.printf("f: %24.20f%n", f);
+        System.out.printf("f2: %24.20f%n", f2);
+        System.out.printf("d: %24.20f%n%n", d);
+        System.out.println("bf: "+ b);  //  BigDecimal
+        System.out.println("bf: "+ b);
+        System.out.println("bf2: "+ b);
+        System.out.println("bd: "+ b);
+    }
+}
